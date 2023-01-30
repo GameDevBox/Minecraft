d0778058b76c933b1942a238507ce01cb6dfac40
GolomOder <golomodersup@gmail.com>
2023-01-30 23:47:15 +0330

    Add files via upload

---
+++ P13/Chunk.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk
+{
+    public Material cube_Material;
+    public Block[,,] chunkData;
+    public GameObject chunk;
+
+    void buildChunk()
+    {
+        chunkData = new Block[World.chunkSize, World.chunkSize, World.chunkSize];
+
+
+        for (int z = 0; z < World.chunkSize; z++)
+            for (int y = 0; y < World.chunkSize; y++)
+                for (int x = 0; x < World.chunkSize; x++)
+                {
+                    Vector3 pos = new Vector3(x, y, z);
+
+                    int WorldX = (int)(x + chunk.transform.position.x);
+                    int WorldY = (int)(y + chunk.transform.position.y);
+                    int WorldZ = (int)(z + chunk.transform.position.z);
+
+                    if (WorldY <= NoiseUtils.GenerateHeight(WorldX, WorldZ))
+                        chunkData[x, y, z] = new Block(Block.BlockType.DIRT, pos, chunk.gameObject, this);
+                    else
+                        chunkData[x, y, z] = new Block(Block.BlockType.AIR, pos, chunk.gameObject, this);
+                }
+    }
+
+
+    public Chunk(Vector3 position, Material mat)
+    {
+        chunk = new GameObject(World.BuildChunkName(position));
+        chunk.transform.position = position;
+        cube_Material = mat;
+        buildChunk();
+    }
+
+
+    public void DrawChunk()
+    {
+        //Draw block
+        for (int z = 0; z < World.chunkSize; z++)
+            for (int y = 0; y < World.chunkSize; y++)
+                for (int x = 0; x < World.chunkSize; x++)
+                {
+                    chunkData[x, y, z].Draw();
+                }
+
+        CombineQuads();
+    }
+
+    private void CombineQuads()
+    {
+        // Combine all children meshes 
+        MeshFilter[] meshFilters = chunk.GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            i++;
+        }
+
+
+        // create a new mesh on the parent object + Add Combined mesh to it
+        MeshFilter mf = (MeshFilter)chunk.gameObject.AddComponent(typeof(MeshFilter));
+        mf.mesh = new Mesh();
+
+        mf.mesh.CombineMeshes(combine);
+
+
+        // Create renderer for the parent 
+        MeshRenderer meshRenderer = chunk.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+        meshRenderer.material = cube_Material;
+
+
+        // Delete all uncombined children
+        foreach (Transform quad in chunk.transform)
+        {
+            GameObject.Destroy(quad.gameObject);
+        }
+    }
+}
---
+++ P13/NoiseUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseUtils {
+
+	static int maxHeight = 150;
+	static float smooth = 0.01f;
+	static int octaves = 4;
+	static float persistence = 0.5f;
+
+	public static int GenerateHeight(float x, float z)
+	{
+		float height = Map(0,maxHeight, 0, 1, fBM(x*smooth,z*smooth,octaves,persistence));
+		return (int) height;
+	}
+
+
+	private static float Map(float newmin, float newmax, float originmin, float originmax, float value)
+    {
+        return Mathf.Lerp (newmin, newmax, Mathf.InverseLerp (originmin, originmax, value));
+    }
+
+
+    private static float fBM(float x, float z, int oct, float pers)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+        for(int i = 0; i < oct ; i++) 
+        {
+                total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+
+                maxValue += amplitude;
+
+                amplitude *= pers;
+                frequency *= 2;
+        }
+
+        return total/maxValue;
+    }
+}
