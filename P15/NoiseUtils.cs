68d9c0355b5c15070bf827a25b56cf71ce1b4604
GolomOder <golomodersup@gmail.com>
2023-02-01 20:05:06 +0330

    Add files via upload

---
+++ P15/NoiseUtils.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseUtils
+{
+
+    static int maxHeight = 150;
+    static float smooth = 0.01f;
+    static int octaves = 4;
+    static float persistence = 0.5f;
+
+    public static int GenerateStoneHeight(float x, float z)
+    {
+        float height = Map(0, maxHeight - 5, 0, 1, fBM(x * smooth * 2, z * smooth * 2, octaves + 1, persistence));
+        return (int)height;
+    }
+
+    public static int GenerateHeight(float x, float z)
+    {
+        float height = Map(0, maxHeight, 0, 1, fBM(x * smooth, z * smooth, octaves, persistence));
+        return (int)height;
+    }
+
+
+    private static float Map(float newmin, float newmax, float originmin, float originmax, float value)
+    {
+        return Mathf.Lerp(newmin, newmax, Mathf.InverseLerp(originmin, originmax, value));
+    }
+
+
+    public static float fBM3D(float x, float y, float z, float sm, int oct)
+    {
+        float XY = fBM(x * sm, y * sm, oct, 0.5f);
+        float YZ = fBM(y * sm, z * sm, oct, 0.5f);
+        float XZ = fBM(x * sm, z * sm, oct, 0.5f);
+
+        float YX = fBM(y * sm, x * sm, oct, 0.5f);
+        float ZY = fBM(z * sm, y * sm, oct, 0.5f);
+        float ZX = fBM(z * sm, x * sm, oct, 0.5f);
+
+        return (XY + YZ + XZ + YX + ZY + ZX) / 6.0f;
+
+    }
+
+
+    private static float fBM(float x, float z, int oct, float pers)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+        for (int i = 0; i < oct; i++)
+        {
+            total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+
+            maxValue += amplitude;
+
+            amplitude *= pers;
+            frequency *= 2;
+        }
+
+        return total / maxValue;
+    }
+}
